03acf7a41498ce9618e0f48c94c19b4b89e40ac8
Anthony-1337 <84737002+Anthony-Black-1337@users.noreply.github.com>
2022-11-05 14:39:25 -0300

    Add files via upload

---
+++ TBoUFSM/html/src/com/test/game/client/HtmlLauncher.java
@@ -0,0 +1,24 @@
+package com.test.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.test.game.Teste;
+
+import java.awt.Window;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(720, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Teste();
+        }
+}
